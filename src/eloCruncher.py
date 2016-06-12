3235f1e1028ce2793b3a7076bbb778667c164770
Dylan Eustice <Dylan.Eustice@gmail.com>
2016-06-12 15:50:08 -0600

    Added rating adjuster, cosmetic changes,  started runout elos function

--- src/eloCruncher.py
+++ src/eloCruncher.py
@@ -12,11 +12,16 @@ class Team:
 		self.name = teamid_dict[str(int(tid))]
 		self.games = games
 		self.scores = np.array([games['this_Score'], games['other_Score']])
-		self.game_idx = np.array(games['Id'])
+		self.tids = np.array([games['this_TeamId'], games['other_TeamId']])
+		self.gids = np.array(games['Id'])
+		self.dates = np.sort(games['DateUtc'])
+
+	def __eq__(self, tid):
+		return tid == self.tid
 
 	def get_game(self, gid):
-		if gid in self.game_idx:
-			return self.scores[:,self.game_idx==gid]
+		if gid in self.gids:
+			return self.scores[:,self.gids==gid]
 		else:
 			print "Game ID {} not found for {}".format(gid, self.name)
 			return None
@@ -35,3 +40,28 @@ def build_all_teams():
 		this_games = all_data[all_data['this_TeamId'] == tid]
 		teams.append(Team(tid, this_name, this_games))
 	return teams
+
+
+def rating_adjuster(Ri, A, B, C, K, elo_diff, MoV):
+	"""
+	Adjust a team's Elo rating based on the outcome of the game
+	A, B, C, K:	Parameters
+	Ri:			Initial rating
+	MoV:		Margin of victory (this_score - other_score)
+	elo_diff:	Elo delta (elo_winner - elo_loser)
+	"""
+	MoV_mult = A / (B + C * elo_diff)
+	MoV_adj = np.sign(MoV) * np.log(np.abs(MoV) + 1)
+	return Ri + K * MoV_adj * MoV_mult
+
+
+def run_all_elos(init_elo, teams, A, B, C, K):
+	"""
+	"""
+	# First interleave all games, sorting by date
+	shf_dates = np.concatenate([t.dates for t in teams])
+	date_idx = np.argsort(shf_dates)
+	scores = np.concatenate([t.scores for t in teams], axis=1)[:,date_idx]
+	gids = np.concatenate([t.gids for t in teams], axis=1)[:,date_idx]
+	tids = np.concatenate([t.tids for t in teams], axis=1)[:,date_idx]
+
