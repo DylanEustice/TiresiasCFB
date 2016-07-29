f14d43b7cb23187ddd3c0814bc685f1458f71a44
Dylan Eustice <Dylan.Eustice@gmail.com>
2016-07-28 23:34:45 -0600

    Fancy plotting stuff

--- src/eloCruncher.py
+++ src/eloCruncher.py
@@ -47,7 +47,7 @@ class Team:
 			print "Game ID {} not found for {}".format(gid, self.name)
 			return None
 
-	def plot_elo(self, elo_dict, ax=None, ix=None):
+	def plot_elo(self, elo_dict, ax=None, ix=None, **kwargs):
 		if ax is None:
 			fig = plt.figure()
 			ax = fig.add_subplot(111)
@@ -57,7 +57,7 @@ class Team:
 			team_elos = [e[ix] for seas in elo_dict[self.tid] for e in seas]
 		c1 = self.info['PrimaryColor']
 		c2 = self.info['SecondaryColor']
-		ax.plot(team_elos, '-o', markerfacecolor=c1, markeredgecolor=c2, color=c2)
+		ax.plot(team_elos, '-o', markerfacecolor=c1, markeredgecolor=c2, color=c2, **kwargs)
 		ax.grid('on')
 		return ax
 
