9b07afc2652e7ca735a6e723675a4a38a5392f6d
Dylan Eustice <Dylan.Eustice@gmail.com>
2016-08-26 21:26:11 -0600

    Default parameters module

---
+++ src/default_parameters.py
@@ -0,0 +1,10 @@
+schedule_columns = [
+	'Id', 'this_TeamId', 'other_TeamId', 'DateUtc', 'Season',
+	'Week', 'Spread', 'OverUnder', 'is_home'
+]
+
+season_day_sep = 100
+this_year = 2016
+
+# Directories
+ comp_team_dir = os.path.join('data', 'compiled_team_data')
