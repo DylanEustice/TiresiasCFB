75f355151599a433d8f3b0d2d71e7829ac02ab8c
Dylan Eustice <Dylan.Eustice@gmail.com>
2016-08-28 14:36:57 -0600

    Added pass and rush elos and can grab elos from any date

--- src/team.py
+++ src/team.py
@@ -4,55 +4,75 @@ import os
 import datetime
 import src.default_parameters as default
 import numpy as np
+from dateutil import parser
+import pytz
 
 
 class Team:
-	def __init__(self, tid, name, games, years, schedule):
+	def __init__(self, tid, name, games, schedule):
 		self.tid = tid
 		self.name = name
 		self.games = games.sort_values(by='DateUtc')
-		self.seasons = years
+		self.seasons = np.unique(self.games['Season'])
 		self.schedule = schedule.sort_values(by='DateUtc')
 		self.info = {}
 		for year in self.seasons:
 			team_dir = os.path.join('data', str(int(year)), 'teams')
 			try:
 				self.info[year] = util.load_json(self.name + '.json', fdir=team_dir)
-			except:
-				if self.name in default.no_team and year in default.no_team[self.name]:
-					continue
+			except IOError:
 				self.info[year] = util.load_json(default.team_alt_mapping[self.name] + '.json', fdir=team_dir)
 		self._elo_params = {}
 		self._elo_params['this_wl_elo'] = np.loadtxt(os.path.join(default.elo_dir, 'Optimal_Winloss_Params.txt'))
 		self._elo_params['this_off_elo'] = np.loadtxt(os.path.join(default.elo_dir, 'Optimal_Offdef_Params.txt'))
 		self._elo_params['this_def_elo'] = self._elo_params['this_off_elo']
 		self._elo_params['this_cf_elo'] = np.loadtxt(os.path.join(default.elo_dir, 'Optimal_Conf_Params.txt'))
-		self.elos = self._get_current_elos()
+		self._elo_params['this_poff_elo'] = np.loadtxt(os.path.join(default.elo_dir, 'Optimal_PassYd_Params.txt'))
+		self._elo_params['this_pdef_elo'] = self._elo_params['this_poff_elo']
+		self._elo_params['this_roff_elo'] = np.loadtxt(os.path.join(default.elo_dir, 'Optimal_RushYd_Params.txt'))
+		self._elo_params['this_rdef_elo'] = self._elo_params['this_roff_elo']
+		self.elos = self.get_current_elos()
 
 	def __eq__(self, id_):
 		return id_ == self.tid or id_ == self.name
 
-	def _get_current_elos(self):
+	def get_current_elos(self, next_game_date="schedule"):
 		"""
 		"""
-		if self.schedule.shape[0]:
+		elo_fields = default.this_elo_fields
+
+		if next_game_date == "schedule" and self.schedule.shape[0]:
 			next_game_date = self.schedule['DateUtc'].values[0]
-			last_game_date = self.games['DateUtc'].values[-1]
-			date_diff = next_game_date - last_game_date
-			date_diff = datetime.timedelta(seconds=date_diff.astype('O')/1e9)
-			elo_fields = ['this_wl_elo', 'this_cf_elo', 'this_off_elo', 'this_def_elo']
+		elif next_game_date == "schedule":
 			elos = [self.games[f].values[-1] for f in elo_fields]
-			if date_diff > datetime.timedelta(days=100):
-				for i, f in enumerate(elo_fields):
-					season_regress = self._elo_params[f][4]
-					init_elo = self._elo_params[f][5]
-					elos[i] = elos[i] + season_regress*(init_elo - elos[i])
-		elif self.games.shape[0]:
-			elo_fields = ['this_wl_elo', 'this_cf_elo', 'this_off_elo', 'this_def_elo']
-			elos = [self.games[f].values[-1] for f in elo_fields]
+			return elos
 		else:
-			assert False,"Team has no games at all?"
+			assert isinstance(next_game_date, datetime.datetime), "Should be datetime"
+
+		ix_last_game = self._get_prev_game_ix_before_date(next_game_date)
+		last_game_date = self.games['DateUtc'].values[ix_last_game]
+		# Make sure these are datetime objects (super annoying)
+		next_game_date = parser.parse(str(next_game_date)).replace(tzinfo=pytz.UTC)
+		last_game_date = parser.parse(str(last_game_date)).replace(tzinfo=pytz.UTC)
+		date_diff = (next_game_date - last_game_date).total_seconds() / (24*60*60)
+		elos = [self.games[f].values[ix_last_game] for f in elo_fields]
+		if date_diff > default.season_day_sep:
+			elos = self._regress_elos(elos)
+
 		return elos
+
+	def _get_prev_game_ix_before_date(self, date):
+		prev_games = self.games['DateUtc'] < date
+		return np.where(prev_games)[0][-1]
+
+	def _regress_elos(self, elos):
+		elo_fields = default.this_elo_fields
+		for i, f in enumerate(elo_fields):
+			season_regress = self._elo_params[f][4]
+			init_elo = self._elo_params[f][5]
+			elos[i] = elos[i] + season_regress*(init_elo - elos[i])
+		return elos
+
 
 	def get_game(self, gid):
 		"""
@@ -111,7 +131,9 @@ class Team:
 				other_inp_data = prm.inp_avg(other_inp_data_all, **kwargs)
 				inp_data = np.hstack([this_inp_data, other_inp_data])
 				tar_data = build_data_from_games(g, tar_fields)
-				this_game = dict([('inp',inp_data), ('tar',tar_data)])
+				if tar_data.shape[0] > 1:
+					tar_data = tar_data.reshape(1,tar_data.shape[0])
+				this_game = dict([('inp',inp_data), ('tar',tar_data), ('id',g['Id'])])
 				games.append(this_game)
 		return games
 
@@ -155,5 +177,5 @@ def build_all_teams(years=range(2005,2017), all_data=None):
 		this_schedule = schedule[schedule['this_TeamId'] == tid]
 		has_games_years = any([any(this_games['Season'] == year) for year in years])
 		if this_games.shape[0] > 0 and has_games_years:
-			teams.append(Team(tid, this_name, this_games, years, this_schedule))
+			teams.append(Team(tid, this_name, this_games, this_schedule))
 	return teams
